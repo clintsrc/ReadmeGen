cb3776cbe59eaf274451795f81ea65d4d959f364
Clint Jones <clinton.alan.jones@gmail.com>
2024-11-27 14:48:13 -0800

    Stubbed in some basic wiring... need to refactor then fill it out

--- index.js
+++ index.js
@@ -1,13 +1,67 @@
-// TODO: Include packages needed for this application
+/*
+ * README.md: https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide
+ * 
+ */
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// NPM packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const banner = 
+`==========================================================
+ReadmeGen
+==========================================================
+Welcome to ReadmeGen! You will receive several prompts for 
+information that will be used to dynamically generate a 
+professional README.md file that you can include with your 
+source control repository.
+`;
 
-// TODO: Create a function to initialize app
-function init() {}
+// An array of questions for user input via the inquirer module
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'location',
+        message: 'What is your location?',
+    },
+];
 
-// Function call to initialize app
+// Write the dynamically generated content to the README.md file. 
+// Use a document template to organize and update the README 
+// content with the answers that were provided
+const generateReadme = ({ name, location}) => {
+    let outFileName = "README.md";
+
+    let data = `TODO Hi, my name's ${name}, I'm in ${location}`;
+
+    fs.writeFile(outFileName, data, (err) => {
+        if (! err) {
+            console.log(`Successfully wrote to file! ${outFileName}`);
+        } else {
+            console.log(`Error writing to file! ${outFileName}`);
+        }
+    });
+}
+
+
+// Initialize app to prompt the user for the README content and
+//    to write to the README.md
+function init() {
+    console.log(banner);
+
+    generateMarkdown("");   // TODO
+
+    inquirer
+    .prompt(questions)
+    .then((answers) => generateReadme(answers))
+    .catch((err) => console.error(err));
+}
+
+// Function call to initialize app.
 init();
