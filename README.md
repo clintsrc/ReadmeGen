2006fadfb9f5b77360193476f5c78c54d167e14c
Clint Jones <clinton.alan.jones@gmail.com>
2024-12-24 12:19:54 -0800

    MVP README.md for the new HR 1985 challenge 10

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
-# ReadmeGen [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# HR1985 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 ## Description
 
-ReadmeGen uses nodejs and the "inquirer" dependency package to prompt you for information used to generate a README.md file for your GitHub project.
+__HR 1985__ is an application that tracks employee roles and department information in a [PostgreSQL](https://www.postgresql.org/) database. Database access is driven through command line input using the node [pg](https://www.npmjs.com/package/pg) and [inquirer](https://www.npmjs.com/package/inquirer/v/8.2.4) packages
 
 ## Table of Contents
 
@@ -19,15 +19,16 @@ ReadmeGen uses nodejs and the "inquirer" dependency package to prompt you for in
 
 1. Change the project's root directory  
 2. Install the dependency modules: npm install  
-![ReadmeGen screenshot](assets/images/screenshot.png)
+3. Build using: npm run build  
+4. Set your postgres credentials in the .env file (see the .env.EXAMPLE FILE)  
+5. Install the schema: pgsql -U postgres -f db/schema.sql  
+6. __Optional__ seed the database with test data: pgsql -U postgres -f db/seeds.sql
 
 ## Usage
 
-1. Run: node src/index.js  
-2. Answer the prompts  
-3. View the generated README.md  
-* See the [ReadmeGen Walkthrough](https://drive.google.com/file/d/1LaERyMDhP6-J8q0OTHyf95QkQvJzfBkp/view)  
-* Here's the walkthrough [output](examples/README.md) file
+See the walkthrough video [here](https://drive.google.com/file/d/1cJHOSW0YCQAJmIs3dNgBczkvHlMfWcpE/view?usp=drive_link)!  
+1. Build and run the application from the project root directory: npm start.  
+2. Follow the menu prompts.
 
 ## License
 
@@ -40,13 +41,14 @@ Ensure your code follows the project's coding standards.
 Write clear and concise commit messages.  
 If your changes include new features, please update the documentation accordingly.  
 If you are fixing a bug, please include a test to verify the fix.  
+  
 Thank you for your contributions!
 
 ## Tests
 
 Test instructions:  
-1. Try generating a README with license, and another that has no license (select None).  
-2. For the contact questions try these account entry combinations: both, neither, github only, email only
+1. Try each of the menu items.  
+2. Make sure that any data you add is correctly reflected in all relevant screens that display the current data
 
 ## Questions
 
--- src/index.js
+++ src/index.js
@@ -34,11 +34,11 @@ let defaultAnswers = [];
 if (DEBUG) {
     defaultAnswers = [
         'HR1985',
-        '__HR 1985__ is an application that tracks employee roles and department information in a PostgreSQL database. Database access is driven through command line input using the node pg and inquirer packages',
-        '1. Change the project\'s root directory  2. Install the dependency modules: npm install  3. Build using: npm run build  4. Set your postgres credentials in the .env file (see the .env.EXAMPLE FILE) 5. Install the schema: pgsql -U postgres -f db/schema.sql  6. __Optional__ seed the database with test data: pgsql -U postgres -f db/seeds.sql',
-        'See the walkthrough video here!  1. Build and run the application from the project root directory: npm run start.  2. Follow the menu prompts.',
+        '__HR 1985__ is an application that tracks employee roles and department information in a [PostgreSQL](https://www.postgresql.org/) database. Database access is driven through command line input using the node [pg](https://www.npmjs.com/package/pg) and [inquirer](https://www.npmjs.com/package/inquirer/v/8.2.4) packages',
+        '1. Change the project\'s root directory  2. Install the dependency modules: npm install  3. Build using: npm run build  4. Set your postgres credentials in the .env file (see the .env.EXAMPLE FILE)  5. Install the schema: pgsql -U postgres -f db/schema.sql  6. __Optional__ seed the database with test data: pgsql -U postgres -f db/seeds.sql',
+        'See the walkthrough video [here](https://drive.google.com/file/d/1cJHOSW0YCQAJmIs3dNgBczkvHlMfWcpE/view?usp=drive_link)!  1. Build and run the application from the project root directory: npm start.  2. Follow the menu prompts.',
         'mit',
-        'Guidelines:  Ensure your code follows the project\'s coding standards.  Write clear and concise commit messages.  If your changes include new features, please update the documentation accordingly.  If you are fixing a bug, please include a test to verify the fix.    Thank you for your contributions!    Thanks to [Justin Moore](https://github.com/Jmo5896) for help with a deployment issue and some of the data formatting.',
+        'Guidelines:  Ensure your code follows the project\'s coding standards.  Write clear and concise commit messages.  If your changes include new features, please update the documentation accordingly.  If you are fixing a bug, please include a test to verify the fix.    Thank you for your contributions!',
         'Test instructions:  1. Try each of the menu items.  2. Make sure that any data you add is correctly reflected in all relevant screens that display the current data',
         'clintsrc',
         'clinton.alan.jones@gmail.com',
