710ccb5ec67147c05e7080b751cddca829629edd
Clint Jones <clinton.alan.jones@gmail.com>
2025-02-21 15:51:06 -0800

    Initial changes for challenge 17

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 
-# ReadmeGen [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# SNAPI [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 ## Description
 
-ReadmeGen uses nodejs and the "inquirer" dependency package to prompt you for information used to generate a README.md file for your GitHub project.
+SNAPI simulates a social network web application where users can share their thoughts, react to friends' thoughts, and create a friend list. It uses Express.js for routing, a MongoDB database, the Mongoose ODM and displays timestamps using the dayjs library.
 
 ## Table of Contents
 
@@ -19,15 +19,15 @@ ReadmeGen uses nodejs and the "inquirer" dependency package to prompt you for in
 
 1. Change the project's root directory  
 2. Install the dependency modules: npm install  
-![ReadmeGen screenshot](assets/images/screenshot.png)
+3. Build: npm run build  
+![SNAPI screenshot](Assets/images/screenshot.png)
 
 ## Usage
 
-1. Run: node src/index.js  
-2. Answer the prompts  
-3. View the generated README.md  
-* See the [ReadmeGen Walkthrough](https://drive.google.com/file/d/1LaERyMDhP6-J8q0OTHyf95QkQvJzfBkp/view)  
-* Here's the walkthrough [output](examples/README.md) file
+1. Start the server: npm run start  
+2. See the ![spec](Assets/decs/spec.md) for details of the original requirements  
+3. Use a RESTful API tool to send HTTP requests. For examples you can import this ![Insomnia test](Assets/test/SNAPI-REST.ext) configuration  
+* See the [SNAPI Walkthrough video](https://drive.google.com/TBD)
 
 ## License
 
@@ -45,8 +45,8 @@ Thank you for your contributions!
 ## Tests
 
 Test instructions:  
-1. Try generating a README with license, and another that has no license (select None).  
-2. For the contact questions try these account entry combinations: both, neither, github only, email only
+1. Add to the existing HTTP requests in the Insomnia configuration.  
+2. Test any new CRUD routes you add and execute the existing suite. Pay particular attention to any existing routes that would be effected by your changes.
 
 ## Questions
 
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 import generateMarkdown from './utils/generateMarkdown.js';
 
-const DEBUG = false;
+const DEBUG = true;
 
 const banner =
     `
@@ -33,13 +33,13 @@ let defaultAnswers = [];
 
 if (DEBUG) {
     defaultAnswers = [
-        'ReadmeGen',
-        'ReadmeGen uses nodejs and the "inquirer" dependency package to prompt you for information used to generate a README.md file for your GitHub project.',
-        '1. Change the project\'s root directory  2. Install the dependency modules: npm install  ![ReadmeGen screenshot](assets/images/screenshot.png)',
-        '1. Run: node src/index.js  2. Answer the prompts  3. View the generated README.md  * See the [ReadmeGen Walkthrough](https://drive.google.com/file/d/1LaERyMDhP6-J8q0OTHyf95QkQvJzfBkp/view)  * Here\'s the walkthrough [output](examples/README.md) file',
+        'SNAPI',
+        'SNAPI simulates a social network web application where users can share their thoughts, react to friends\' thoughts, and create a friend list. It uses Express.js for routing, a MongoDB database along with the Mongoose ODM, and displays timestamps using the dayjs library.',
+        '1. Change the project\'s root directory  2. Install the dependency modules: npm install  3. Build: npm run build  ![SNAPI screenshot](Assets/images/screenshot.png)',
+        '1. Start the server: npm run start  2. See the ![spec](Assets/decs/spec.md) for details of the original requirements  3. Use a RESTful API tool to send HTTP requests. For examples you can import this ![Insomnia test](Assets/test/SNAPI-REST.ext) configuration  * See the [SNAPI Walkthrough video](https://drive.google.com/TBD)',
         'mit',
         'Guidelines:  Ensure your code follows the project\'s coding standards.  Write clear and concise commit messages.  If your changes include new features, please update the documentation accordingly.  If you are fixing a bug, please include a test to verify the fix.  Thank you for your contributions!',
-        'Test instructions:  1. Try generating a README with license, and another that has no license (select None).  2. For the contact questions try these account entry combinations: both, neither, github only, email only',
+        'Test instructions:  1. Add to the existing HTTP requests in the Insomnia configuration.  2. Test any new CRUD routes you add and execute the existing suite. Pay particular attention to any existing routes that would be effected by your changes.',
         'clintsrc',
         'clinton.alan.jones@gmail.com',
     ];
