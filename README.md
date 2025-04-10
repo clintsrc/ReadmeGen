a76e7311dbafd53f64729d7a00a4e67b788e9b2f
Clint Jones <clinton.alan.jones@gmail.com>
2025-04-10 12:32:07 -0700

    First draft

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 
-# ReadmeGen [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+# Cash Is King CRM [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 ## Description
 
-ReadmeGen uses nodejs and the "inquirer" dependency package to prompt you for information used to generate a README.md file for your GitHub project.
+Cash Is King CRM is an application used to organize ATM rentals for events.  
+  
+[![MongoDB](https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white)](https://www.mongodb.com/) [![Express.js](https://img.shields.io/badge/Express%20js-000000?style=for-the-badge&logo=express&logoColor=white)](https://expressjs.com/) [![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)](https://react.dev/) [![Node.js](https://img.shields.io/badge/Node%20js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)](https://nodejs.org/en)  
+[![JWT](https://img.shields.io/badge/JWT-000000?style=for-the-badge&logo=JSON%20web%20tokens&logoColor=white)](https://www.npmjs.com/package/jsonwebtoken) [![Apollo GraphQL](https://img.shields.io/badge/Apollo%20GraphQL-311C87?&style=for-the-badge&logo=Apollo%20GraphQL&logoColor=white)](https://www.apollographql.com/) [![Cypress](https://img.shields.io/badge/Cypress-17202C?style=for-the-badge&logo=cypress&logoColor=white)](https://www.cypress.io/)  
+[![Font Awesome](https://img.shields.io/badge/Font_Awesome-339AF0?style=for-the-badge&logo=fontawesome&logoColor=white)](https://fontawesome.com/) [![Render](https://img.shields.io/badge/Render-CI/CD-blue)](https://dashboard.render.com/)
 
 ## Table of Contents
 
@@ -17,17 +21,22 @@ ReadmeGen uses nodejs and the "inquirer" dependency package to prompt you for in
 
 ## Installation
 
-1. Change the project's root directory  
+1. Change to the project's root directory  
 2. Install the dependency modules: npm install  
-![ReadmeGen screenshot](assets/images/screenshot.png)
+3. Build: npm run build  
+4. Configure the MongoDB database:  
+  
+- Create a server/.env (refer to the .env.EXAMPLE located there)  
+5. Seed the database: npm run seed
 
 ## Usage
 
-1. Run: npm run build  
-2. Answer the prompts  
-3. View the generated README.md  
-* See the [ReadmeGen Walkthrough](https://drive.google.com/file/d/1LaERyMDhP6-J8q0OTHyf95QkQvJzfBkp/view)  
-* Here's the walkthrough [output](examples/README.md) file
+1. Change to the project's root directory  
+2. Start the application: npm run dev  
+3. Refer to the [Tests](#tests) section to execute the Cypress tests.  
+4. See the [spec](client/assets/docs/spec.md) for details of the original requirements  
+* See the active site deployed on Render [here](https://cash-is-king-crm.onrender.com/)  
+![Cash Is King CRM screenshot](client/assets/images/screenshot.png)
 
 ## License
 
@@ -45,8 +54,10 @@ Thank you for your contributions!
 ## Tests
 
 Test instructions:  
-1. Try generating a README with license, and another that has no license (select None).  
-2. For the contact questions try these account entry combinations: both, neither, github only, email only
+1. Change to the project's root directory  
+2. Cypress requires the application under test to be running (see [Usage](#usage))  
+3. Run the tests in console mode: npm run test  
+4. To launch the test runner UI instead use: npm run test-gui
 
 ## Questions
 
--- src/index.js
+++ src/index.js
@@ -20,9 +20,9 @@ const banner = `
 ==========================================================
 ReadmeGen
 ==========================================================
-Welcome to ReadmeGen! You will receive several prompts for 
-information that will be used to dynamically generate a 
-professional README.md file that you can include with your 
+Welcome to ReadmeGen! You will receive several prompts for
+information that will be used to dynamically generate a
+professional README.md file that you can include with your
 source control repository. Use double-spaces to indicate
 line breaks.
 `;
@@ -32,10 +32,10 @@ let defaultAnswers = [];
 
 if (DEBUG) {
   defaultAnswers = [
-    "Solid Guess",
-    "Solid Guess is a MERN application that has been enhanced with Cypress end-to-end and component testing.    [![MongoDB](https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white)](https://www.mongodb.com/) [![Express.js](https://img.shields.io/badge/Express%20js-000000?style=for-the-badge&logo=express&logoColor=white)](https://expressjs.com/) [![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)](https://react.dev/) [![Node.js](https://img.shields.io/badge/Node%20js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)](https://nodejs.org/en)  [![Cypress](https://img.shields.io/badge/Cypress-17202C?style=for-the-badge&logo=cypress&logoColor=white)](https://www.cypress.io/)  [![Font Awesome](https://img.shields.io/badge/Font_Awesome-339AF0?style=for-the-badge&logo=fontawesome&logoColor=white)](https://fontawesome.com/)  ",
+    "Cash Is King CRM",
+    "Cash Is King CRM is an application used to organize ATM rentals for events.    [![MongoDB](https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white)](https://www.mongodb.com/) [![Express.js](https://img.shields.io/badge/Express%20js-000000?style=for-the-badge&logo=express&logoColor=white)](https://expressjs.com/) [![React](https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB)](https://react.dev/) [![Node.js](https://img.shields.io/badge/Node%20js-339933?style=for-the-badge&logo=nodedotjs&logoColor=white)](https://nodejs.org/en)  [![JWT](https://img.shields.io/badge/JWT-000000?style=for-the-badge&logo=JSON%20web%20tokens&logoColor=white)](https://www.npmjs.com/package/jsonwebtoken) [![Apollo GraphQL](https://img.shields.io/badge/Apollo%20GraphQL-311C87?&style=for-the-badge&logo=Apollo%20GraphQL&logoColor=white)](https://www.apollographql.com/) [![Cypress](https://img.shields.io/badge/Cypress-17202C?style=for-the-badge&logo=cypress&logoColor=white)](https://www.cypress.io/)  [![Font Awesome](https://img.shields.io/badge/Font_Awesome-339AF0?style=for-the-badge&logo=fontawesome&logoColor=white)](https://fontawesome.com/) [![Render](https://img.shields.io/badge/Render-CI/CD-blue)](https://dashboard.render.com/)",
     "1. Change to the project's root directory  2. Install the dependency modules: npm install  3. Build: npm run build  4. Configure the MongoDB database:    - Create a server/.env (refer to the .env.EXAMPLE located there)  5. Seed the database: npm run seed",
-    "1. Change to the project's root directory  2. Start the application: npm run start:dev  3. Refer to the [Tests](#tests) section to execute the Cypress tests.  4. See the [spec](client/assets/docs/spec.md) for details of the original requirements  * See the [Solid Guess Walkthrough video](https://drive.google.com/file/d/1xETFAwS45cJ-MPmlHr3GEqDM5AXR7gRr/view?usp=drive_link)  * ![Solid Guess screenshot](client/assets/images/screenshot.png)",
+    "1. Change to the project's root directory  2. Start the application: npm run dev  3. Refer to the [Tests](#tests) section to execute the Cypress tests.  4. See the [spec](client/assets/docs/spec.md) for details of the original requirements  * See the active site deployed on Render [here](https://cash-is-king-crm.onrender.com/)  ![Cash Is King CRM screenshot](client/assets/images/screenshot.png)",
     "mit",
     "Guidelines:  Ensure your code follows the project's coding standards.  Write clear and concise commit messages.  If your changes include new features, please update the documentation accordingly.  If you are fixing a bug, please include a test to verify the fix.  Thank you for your contributions!",
     "Test instructions:  1. Change to the project's root directory  2. Cypress requires the application under test to be running (see [Usage](#usage))  3. Run the tests in console mode: npm run test  4. To launch the test runner UI instead use: npm run test-gui",
